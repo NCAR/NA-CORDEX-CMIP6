dc9ee5823f0b4692cc4453ca5af91873e11f0c62
Jacob Stuivenvolt-Allen <jsallen@crlogin2.hpc.ucar.edu>
2026-04-16 16:01:21 -0600

    New config_env.sh file for custom conda env with thermofeel

--- postprocess/config_env.sh
+++ postprocess/config_env.sh
@@ -3,3 +3,7 @@ module load nco
 module load cdo
 module load conda
 conda activate na_cordex
+
+which python
+python -c "import sys; print(sys.executable)"
+python -c "import thermofeel; print(thermofeel.__file__)" || echo "NO THERMOFEEL"
